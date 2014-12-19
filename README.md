11ebd6f1c003c70199fce00272524fa225185f5d
Karson Enns <hello@karsonenns.com>
2014-12-18 20:55:04 -0800

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-AshleyRingham.com
-=================
---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/css"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
+
+output_style = :expanded
+
+relative_assets = true
+
+line_comments = false
+
+preferred_syntax = :scss
