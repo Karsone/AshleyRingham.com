a047b016ab198b05cad292b829e779a9379f4e8f
Karsone <hello@karsonenns.com>
2016-03-29 21:19:24 -0700

    Added CSS animations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,26 @@
 base = {
-  jqNeeds:function(){
+  fullHeight:function(){
     var windowHeight = $(window).height();
     $(".full-height").height(windowHeight);
+  },
+  navbar: function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+      } else {
+        $('nav').removeClass('shrink');
+      }
+    });
+  },
+  blockLink:function(){
+    $(document).on("click","[href]", function() {
+      location.href = $(this).attr("href");
+    });
   }
 }
 
 $(document).ready(function(){
-  base.jqNeeds();
+  base.fullHeight();
+  base.navbar();
+  base.blockLink();
 })
